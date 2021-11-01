685d8e195ea35cc1fc2290f9ac4f0e9e971215bd
sadulla <ubaydullayevsadulla843@gmail.com>
2021-11-01 15:29:59 +0500

    Web view

---
+++ app/src/main/java/com/example/googlewebview/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.googlewebview
+
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.webkit.WebViewClient
+import androidx.annotation.RequiresApi
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    @RequiresApi(Build.VERSION_CODES.O)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        setUpWebView()
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun setUpWebView() {
+        web_view.webViewClient = WebViewClient()
+        web_view.apply {
+            loadUrl("https://www.google.com/")
+            settings.javaScriptEnabled = true
+            settings.safeBrowsingEnabled = true
+        }
+    }
+}
